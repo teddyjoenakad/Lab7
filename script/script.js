8c426373aa06a13db2378a205ad384d94ccdd05e
Teddy Nakad <tnakad@my.bcit.ca>
2023-03-10 16:46:02 -0800

    Do while loop for the user input

--- script/script.js
+++ script/script.js
@@ -1,1 +1,6 @@
 const courseList = [{ code:'ACIT 1620',  name:'Web Fundamental Technologies'}, {code:'ACIT 1515', name:'Scripting for IT'}, {code:'MATH 1310', name:'Technical Math for IT'}]
+
+do {
+    user= prompt('Please enter the 4 digit code of the course you are taking: ')
+} 
+while (isNaN(user) == true || user.length != 4)
